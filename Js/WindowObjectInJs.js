6a6936f0d30341272e0eeac099396adbcc370ef6
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-08-03 22:36:22 +0530

    about window object

---
+++ Js/WindowObjectInJs.js
@@ -0,0 +1,14 @@
+console.log("Learn about DOM in js");
+let a = window;
+// alert('Alert!')
+
+// a = prompt("Don't exit without save your work")
+// a = confirm("Are you sure you want to exit")
+a = window.innerHeight;
+a = window.innerWidth;
+a = scrollX;
+a = scrollY;
+a = location;
+a = location.toString(); // gives Location
+a = window.history;
+console.log(a);
