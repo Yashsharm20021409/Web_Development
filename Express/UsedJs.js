b1bb13e888c75c7b8106b1f441ea644e9e5d174c
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-08-23 15:16:04 +0530

    add CSS+Js+Html and Learn about how to store user input data from form to files

--- Express/UsedJs.js
+++ Express/UsedJs.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = 80;
 
 //- EXPRESS Specific configuration 
 app.use('/static',express.static('static'));
+app.use(express.urlencoded());
 
 //- PUG Specific configuration
 
@@ -20,6 +22,23 @@ app.get('/',(req,res)=>{
     const var1 = {'title':'pubg is the best game',"content":con}
     res.status(200).render('index.pug',var1);
 })
+
+app.post('/',(req,res)=>{
+    // console.log(req.body); // make object and print the user data
+
+    // data is always excess using name not by id or class name
+    name1 = req.body.name;
+    age = req.body.age;
+    Gender = req.body.Gender;
+    Email = req.body.Email;
+    address = req.body.address;
+    more = req.body.more;
+    let UserData = `The Name of The client is ${name1},${age} year old,residing at ${address}.More about him/her ${more},contact info about him/her ${Email}`;
+    fs.writeFileSync('Output.txt',UserData);
+
+    const var1 = {'message':'Form Submit Successfully'};
+    res.status(200).render('index.pug',var1);
+});
 
 //- Star the server
 app.listen(port,()=>{
